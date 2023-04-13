419a76e4321246a34769af9c7fc9fd75e9c35ed5
Alperen Sürmeli <alperensr36@gmail.com>
2023-04-13 15:00:49 +0300

    Continuation of codes.

---
+++ Exceptions/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ExceptionIntro();
+            try
+            {
+            Find();
+            }
+            catch(RecordNotFoundException exception) 
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            HandleException(() =>
+            {
+                Find();
+            });
+           
+
+            Console.ReadLine();
+        }
+
+        private static void HandleException(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        private static void Find()
+        {
+            List<string> students = new List<string> { "Alperen", "Simge", "Mantı" };
+            if (!students.Contains("Ahmet"))
+            {
+                throw new RecordNotFoundException("Record Not Found!");
+            }
+            else
+            {
+                Console.WriteLine("Record Found!");
+            }
+        }
+
+        private static void ExceptionIntro()
+        {
+            try
+            {
+                string[] students = new string[3] { "Alperen", "Simge", "Sude" };
+                students[3] = "Sahra";
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
